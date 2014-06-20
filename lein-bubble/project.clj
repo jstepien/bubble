416242a66643d89c94b73c589c105b2da4733c18
Jan Stępień <jstepien@users.sourceforge.net>
2014-06-20 08:41:33 +0200

    lein-bubble doesn't depend on bubble itself
    It just invokes it on a remote VM.

--- lein-bubble/project.clj
+++ lein-bubble/project.clj
@@ -1,5 +1,4 @@
 (defproject lein-bubble "0.0.0-SNAPSHOT"
-  :dependencies [[bubble "0.0.0-SNAPSHOT"]
-                 [org.clojure/tools.nrepl "0.2.3"
+  :dependencies [[org.clojure/tools.nrepl "0.2.3"
                   :exclusions [org.clojure/clojure]]]
   :eval-in-leiningen true)
